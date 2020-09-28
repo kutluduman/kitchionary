a7e12a4da99569ef0984cbfd65053362964a9a13
kutluduman <kutluduman@gmail.com>
2020-09-28 11:55:43 -0400

    changed backend express back to previous state

--- express-backend/server.js
+++ express-backend/server.js
@@ -34,12 +34,16 @@ app.use(express.static("public"));
 // Separated Routes for each Resource
 // Note: Feel free to replace the example routes below with your own
 const usersRoutes = require("./routes/users");
+const loginRoutes = require("./routes/login");
+const registerRoutes = require("./routes/register");
 
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
 // app.use("/api/users", usersRoutes(db));
-app.use("", usersRoutes(db));
+// app.use("", usersRoutes(db));
+app.use("/register", registerRoutes(db));
+app.use("/login",loginRoutes(db));
 
 // Note: mount other resources here, using the same pattern above
 
