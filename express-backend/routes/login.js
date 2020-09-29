6a7466e5df4d9e7fdd5790a9ee06b0ffa41f8700
Victoria Lee <victorialee72@live.com>
2020-09-28 20:03:23 -0400

    Got login functionality working

--- express-backend/routes/login.js
+++ express-backend/routes/login.js
@@ -3,18 +3,21 @@ const router  = express.Router();
 
 module.exports = (db) => {
 
-  router.post("/login", (req,res) => {
-    const values = [];
-    for(key in req.body) {
-      values.push(req.body[key]);
-    }
-    db.query(`SELECT id, first_name, last_name, email, phone_number
+  router.post("/", (req,res) => {
+    const email = req.body.user.email;
+    const password = req.body.user.password;
+
+    return db.query(`SELECT id, first_name, last_name, email, phone_number
     FROM users
-    WHERE email = $1 AND password = $2;`,values)
+    WHERE email = $1 AND password = $2;`,[email, password])
     .then((data) => {
-      user = data.rows[0];
-      console.log(user);
-      res.json({ user });
+      if (data.rows.length === 0) {
+        res.status(401).send("Incorrect Email or Password");
+      } else {
+        const user = data.rows[0];
+        console.log('user', user);
+        res.json({ user });
+      }
     })
     .catch((err) => {
       res.status(500).json({ error: err.message });
