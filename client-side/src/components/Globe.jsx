df20ab847f2ba608e45dcc9e4966ce00875c15ee
MonicaLuca <monica.lucarini28@gmail.com>
2020-09-30 10:58:08 -0400

    fully formatted home page

--- client-side/src/components/Globe.jsx
+++ client-side/src/components/Globe.jsx
@@ -3,6 +3,7 @@ import "./globe.css";
 import imageQuestion from "../docs/world_white.jpg";
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 
 import background from "../docs/white_back.jpg"
 
@@ -21,14 +22,24 @@ const Globe = () =>  {
   return (
   <div>
     <div className="container">
-      <img className='background image' src={background} alt="dessert" width="450" height="700" />
+      <img className='background image' src={background} alt="dessert" width="450" height="825" />
       <img className="globe image" src={imageQuestion} alt="globe" width="300" height="325"/>
     </div>
     <div className="descriptionGlobe">  
       <h1 className="titleGlobe"> GLOBE MODE</h1>
+    
+    <div className="globeText">
+      <Typography variant="h5" align="center" color="textSecondary" component="h3">
+            Explore recipes to make with ingredients you already have at home!
+            </Typography>  
+</div>
+<br/>
+      <div className="globeButton">
+        
         <Button variant="outlined" color="primary">
           Primary
         </Button>
+        </div>
       </div>
   </div>
   )
