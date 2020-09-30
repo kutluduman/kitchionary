3d4b7a0a5ae9025b8a01f62b57c66b872b9494a1
kutluduman <kutluduman@gmail.com>
2020-09-30 12:34:16 -0400

    created the route for quiz

---
+++ express-backend/routes/quiz.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+
+
+module.exports = (db) => {
+
+
+  router.post("/", (req,res) => {
+
+  })
+
+
+
+
+
+  return router;
+
+}
