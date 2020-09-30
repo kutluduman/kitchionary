f380816b4f1ceeed821a463b6591bfdce544fcac
kutluduman <kutluduman@gmail.com>
2020-09-30 12:41:01 -0400

    added promise and catch for the post request

--- express-backend/routes/quiz.js
+++ express-backend/routes/quiz.js
@@ -7,10 +7,17 @@ module.exports = (db) => {
 
   router.post("/", (req,res) => {
 
+    db.query(``,[])
+    .then((data) => {
+      const values = data.rows;
+      console.log('values', values);
+      res.json({values});
+    })
+    .catch(err => {
+      res.status(500).json({error: err.message});
+    });
+
   })
-
-
-
 
 
   return router;
