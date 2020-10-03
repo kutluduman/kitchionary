d302ad7de5fd149df55704e50ff0981b5800d507
kutluduman <kutluduman@gmail.com>
2020-10-03 12:37:08 -0400

    added the query to render the ratings for recipe

--- express-backend/routes/rating.js
+++ express-backend/routes/rating.js
@@ -1,20 +1,23 @@
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
 
 module.exports = (db) => {
-
-  router.post("/", (req,res) => {
-
-
-    .then((data) => {
-
-    })
-    .catch((err) => {
-      res.status(500).json({ error: err.message });
-    });
+  router.get("/", (req, res) => {
+    return db
+      .query(
+        `SELECT AVG(rating)
+        FROM reviews
+        WHERE recipe_id = 1;`
+      )
+      .then((data) => {
+        const rating = data.rows;
+        console.log("rating", rating);
+        res.json({ rating });
+      })
+      .catch((err) => {
+        res.status(500).json({ error: err.message });
+      });
   });
 
   return router;
-
 };
-
